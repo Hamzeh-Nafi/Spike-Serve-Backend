3cae751ccd340be8754f8caa76849e0933286d33
Hamzeh-Nafi <hamzehnafi@gmail.com>
2026-02-25 19:53:51 +0300

    Added session logic

--- server.js
+++ server.js
@@ -6,9 +6,19 @@ import { serverError } from "./configs/vars.js";
 import helmet from "helmet";
 import cors from "cors";
 import rateLimit from "express-rate-limit";
-const app = express();
+import session from "express-session";
+const app = express()
 
-
+app.use(session({
+  name: "volleyball.sid",
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24 
+  }
+}));
 
 app.use(cors({
     origin: process.env.FRONTEND_ORIGIN,
